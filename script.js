73bb3ad8b7592a384bcd0dfb280f2c3bf9467708
AvaG <avagheana@gmail.com>
2022-11-01 16:49:34 +0200

    done

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const body = document.querySelector('body');
+const label = document.createElement('label');
+const input = document.createElement('input');
+const slider = document.createElement('span');
+
+
+
+body.append(label)
+label.append(input)
+label.append(slider)
+
+
+label.classList.add('switch')
+input.type = 'checkbox'
+slider.classList.add('slider','round')
+
+
+input.addEventListener("change", () => {
+  body.classList.toggle('darktheme')
+  });
+
+
+
