e4e20beebde82dd81040d990d97a510087fbbdbd
AvaG <avagheana@gmail.com>
2022-11-01 17:08:04 +0200

    all done

--- script.js
+++ script.js
@@ -5,18 +5,12 @@ const label = document.createElement('label');
 const input = document.createElement('input');
 const slider = document.createElement('span');
 
-
-
 body.append(label)
 label.append(input)
 label.append(slider)
-
-
 label.classList.add('switch')
 input.type = 'checkbox'
 slider.classList.add('slider','round')
-
-
 input.addEventListener("change", () => {
   body.classList.toggle('darktheme')
   });
